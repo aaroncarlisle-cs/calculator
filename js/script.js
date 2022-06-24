2ec3cbb1decdaf006dfefce41ab16da1da5dd099
Aaron Carlisle <aaroncarlisle.cs@gmail.com>
2022-06-24 13:10:08 -0600

    Create logic for appendDisplay function
    When numeric digits are input by the user, the displayResult node is appended.

--- js/script.js
+++ js/script.js
@@ -1,18 +1,27 @@
+// Global variables for Calculator functions
 let num1;
 let num2;
-let display;
 let operatorSet = false;
+let decimalSet = false;
 let operate = {
     add: function (num1, num2) {return num1 + num2;},
     subtract: function (num1, num2) {return num1 - num2;},
     multiply: function (num1, num2) {return num1 *  num2;},
     divide: function (num1, num2) {return num1 / num2;},
 }
+// Event listeners for Calculator buttons
 const digitBtns = document.querySelectorAll('.digit');
-function attachDigitBtnEvents(digitBtns) {
-    digitBtns.forEach((button) => button.addEventListener('click', () => appendDigit(button.innerText)));
+function attachDigitBtnEvents() {
+    digitBtns.forEach((button) => button.addEventListener('click', appendDisplay));
 }
 
-function appendDisplay(digit) {
-    
+function appendDisplay(button) {
+    let displayResult = document.querySelector('.display-result');
+    let digit = button.target.innerText;
+    displayResult.innerText += digit;
+
+}
+
+window.onload =  () => {
+    attachDigitBtnEvents();
 }
